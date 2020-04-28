723f1ed7dc702b539f22e3d627715cf2ac254e64
giannidoria <gianni.doria@gmail.com>
2020-04-28 14:43:39 +0200

    lib upgrade

--- pd.go
+++ pd.go
@@ -6,26 +6,4 @@ import "github.com/gidor/pd/cmd"
 
 func main() {
 	cmd.Execute()
-	// dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(dir)
-	// // len := len(os.Args)
-	// for i, j := 0, len(os.Args); i < j; i++ {
-	// 	fmt.Println(i, os.Args[i])
-	// }
-	// fmt.Println(dialog.Message("pipo").YesNo())
-	// f, err := dialog.File().Title("sqlite").Filter("Sqlite", "sqlite,sqlite3").Filter("All files", "*").Load()
-	// if err != nil {
-	// 	fmt.Println("ERROR", err)
-	// } else {
-	// 	fmt.Println(f)
-	// }
-	// f, err = dialog.Directory().Title("dir").Browse()
-	// if err != nil {
-	// 	fmt.Println("ERROR", err)
-	// } else {
-	// 	fmt.Println(f)
-	// }
 }
